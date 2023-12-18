6e59e804d0ceeabec499cbe99c9f5475a5d2d768
Caio Barreto <caiobarreto_28@hotmail.com>
2023-12-18 17:03:57 -0300

    feat: products route and home page

---
+++ src/app/api/products/featured/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from 'next/server'
+import data from '../data.json'
+
+export async function GET() {
+  const featuredProducts = data.products.filter((product) => {
+    return product.featured
+  })
+
+  return NextResponse.json({ featuredProducts })
+}
