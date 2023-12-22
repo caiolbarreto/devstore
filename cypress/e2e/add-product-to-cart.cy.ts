0423639463c203ffa37c7b2c513f15ef4c2772db
Caio Barreto <caiobarreto_28@hotmail.com>
2023-12-22 19:31:52 -0300

    feat: add product to cart e2e tests

---
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -0,0 +1,41 @@
+describe('template spec', () => {
+  it('should be able to navigate to the product page and add it to the cart', () => {
+    cy.visit('http://localhost:3000')
+
+    cy.get('a[href^="/product"]').first().click()
+
+    cy.location('pathname').should('include', '/product')
+    cy.contains('Adicionar ao carrinho').click()
+
+    cy.contains('Cart (1)').should('exist')
+  })
+
+  it('should not count duplicated items on cart', () => {
+    cy.visit('http://localhost:3000')
+
+    cy.get('a[href^="/product"]').first().click()
+
+    cy.location('pathname').should('include', '/product')
+
+    cy.contains('Adicionar ao carrinho').click()
+    cy.contains('Adicionar ao carrinho').click()
+
+    cy.contains('Cart (1)').should('exist')
+  })
+
+  it('should be able to search for a product and add it to the cart', () => {
+    cy.visit('http://localhost:3000')
+
+    cy.get('input[name=q]').type('moletom').parent('form').submit()
+
+    cy.location('pathname').should('include', '/search')
+
+    cy.get('a[href^="/product"]').first().click()
+
+    cy.location('pathname').should('include', '/product')
+
+    cy.contains('Adicionar ao carrinho').click()
+
+    cy.contains('Cart (1)').should('exist')
+  })
+})
